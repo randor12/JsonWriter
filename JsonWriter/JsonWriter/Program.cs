b1ef9b33ddece2c0ba9794ef835f5365ab6e39b5
randor12 <rynicholas@vt.edu>
2019-05-22 22:56:39 -0400

    initialize json writer code

---
+++ JsonWriter/JsonWriter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
